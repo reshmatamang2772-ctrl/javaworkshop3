8234b83e09b5277ee1b36946d8bbaec1d9b7a35d
Reshma <reshmatamang2772@gmail.com>
2025-11-20 10:40:46 +0545

    I love chocolates

---
+++ workshopreshma3.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class workshopreshma3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshopreshma3
+{
+    public static void main(String[] args){
+        
+        System.out.println("I love chocolates");
+        
+        
+    
+    }
+}
